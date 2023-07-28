b305a9c28ed93196777edcdb7009a844e64ddbc8
SmallMouth Li <lismallmouth@gmail.com>
2023-07-28 14:45:20 +0800

    add PasswordTextFieldComponent

--- app/src/main/java/com/limouth/mvvm_login/ui/components/AppComponents.kt
+++ app/src/main/java/com/limouth/mvvm_login/ui/components/AppComponents.kt
@@ -3,7 +3,11 @@ package com.limouth.mvvm_login.ui.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
@@ -12,11 +16,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -60,7 +68,8 @@ fun HeadingTextComponent(value: String) {
 
 @Composable
 fun MyTextFieldComponent(
-    labelValue: String
+    labelValue: String,
+    painterResource: Painter
 ) {
 
     val textValue = remember {
@@ -88,8 +97,65 @@ fun MyTextFieldComponent(
         onValueChange = { textValue.value = it },
         keyboardOptions = KeyboardOptions.Default,
         leadingIcon = {
-            Icon(painter = painterResource(id = R.drawable.profile), contentDescription = "")
+            Icon(painter = painterResource, contentDescription = "")
         }
+    )
+
+}
+
+@Composable
+fun PasswordTextFieldComponent(
+    labelValue: String,
+    painterResource: Painter
+) {
+
+    val password = remember {
+        mutableStateOf("")
+    }
+
+    val passwordVisible = remember {
+        mutableStateOf(false)
+    }
+    /*
+    https://stackoverflow.com/questions/67320990/android-jetpack-compose-cant-set-backgroundcolor-for-outlinedtextfield
+    backgroundColor No such parameter anymore in Material3 ,  Use containerColor
+     */
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(componentShapes.small),
+        label = { Text(text = labelValue) },
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedContainerColor = BgColor,
+            unfocusedContainerColor = BgColor,
+            disabledContainerColor = BgColor,
+            cursorColor = Primary,
+            focusedBorderColor = Primary,
+            focusedLabelColor = Primary,
+        ),
+        value = password.value,
+        onValueChange = { password.value = it },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        leadingIcon = {
+            Icon(painter = painterResource, contentDescription = "")
+        },
+        trailingIcon = {
+            val iconImage = if (passwordVisible.value) {
+                Icons.Filled.Visibility
+            } else {
+                Icons.Filled.VisibilityOff
+            }
+            val description = if (passwordVisible.value) {
+                stringResource(id = R.string.hide_password)
+            } else {
+                stringResource(id = R.string.show_password)
+            }
+
+            IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
+                Icon(imageVector = iconImage, contentDescription = description)
+            }
+        },
+        visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation()
     )
 
 }
--- app/src/main/java/com/limouth/mvvm_login/ui/signup/SignUpScreen.kt
+++ app/src/main/java/com/limouth/mvvm_login/ui/signup/SignUpScreen.kt
@@ -2,12 +2,15 @@ package com.limouth.mvvm_login.ui.signup
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,6 +18,7 @@ import com.limouth.mvvm_login.R
 import com.limouth.mvvm_login.ui.components.HeadingTextComponent
 import com.limouth.mvvm_login.ui.components.MyTextFieldComponent
 import com.limouth.mvvm_login.ui.components.NormalTextComponent
+import com.limouth.mvvm_login.ui.components.PasswordTextFieldComponent
 
 @Composable
 fun SignUpScreen() {
@@ -28,8 +32,22 @@ fun SignUpScreen() {
         Column(modifier = Modifier.fillMaxSize()) {
             NormalTextComponent(value = stringResource(R.string.hello))
             HeadingTextComponent(value = stringResource(id = R.string.create_account))
+            Spacer(modifier = Modifier.height(20.dp))
             MyTextFieldComponent(
-                labelValue = stringResource(id = R.string.first_name)
+                labelValue = stringResource(id = R.string.first_name),
+                painterResource(id = R.drawable.profile)
+            )
+            MyTextFieldComponent(
+                labelValue = stringResource(id = R.string.last_name),
+                painterResource = painterResource(id = R.drawable.profile)
+            )
+            MyTextFieldComponent(
+                labelValue = stringResource(id = R.string.email),
+                painterResource = painterResource(id = R.drawable.message)
+            )
+            PasswordTextFieldComponent(
+                labelValue = stringResource(id = R.string.password),
+                painterResource = painterResource(id = R.drawable.ic_lock)
             )
         }
     }
